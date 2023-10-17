0cfafbe685235539a8c88925e1337d378861107b
Birch <birch17261@gmail.com>
2023-10-17 13:17:57 +0800

    feat: update layout

--- front-end/tailwind.config.ts
+++ front-end/tailwind.config.ts
@@ -4,9 +4,27 @@ import { fontFamily } from "tailwindcss/defaultTheme";
 export default {
   content: ["./src/**/*.tsx"],
   theme: {
+    screens: {
+      sm: "100px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+    },
+    lineHeight: {
+      relaxed: "1.8",
+    },
     extend: {
+      colors: {
+        primary: "#fff",
+        foreground: "#d4d4d4",
+        background: "#08070B",
+        input: "#131415",
+        muidground: "#18181B",
+      },
       fontFamily: {
         sans: ["var(--font-sans)", ...fontFamily.sans],
+        poppins: "var(--font-default)",
+        title: "var(--font-title)",
       },
     },
   },
