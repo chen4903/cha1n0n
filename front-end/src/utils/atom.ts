02706da9d5174f5b3d18bd20d803093eab70097f
Birch <birch17261@gmail.com>
2023-10-18 22:47:48 +0800

    chore: test

--- front-end/src/utils/atom.ts
+++ front-end/src/utils/atom.ts
@@ -2,4 +2,5 @@ import { atom } from "jotai";
 import { atomWithStorage } from "jotai/utils";
 
 export const indexAtom = atom(0);
+// test
 export const roleAtom = atomWithStorage("AccountRole", "null");
